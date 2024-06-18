0e0749ebc272951b103cf5ead0727e263c4dbc36
Scott <scott.c.theisen@gmail.com>
2024-06-18 15:25:02 -0400

    First commit

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'AccerateExp-efcb75bed0c64aba9b0ed5e01ac5f766',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
